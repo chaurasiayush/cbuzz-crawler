3c3cc2af17b924895931d5dad663f306b70dda3f
ayushuiet <uiet.ayush@gmail.com>
2020-04-30 20:14:37 +0530

    serieswise scorecard fetching successfull

--- dbmanager.py
+++ dbmanager.py
@@ -1,11 +1,66 @@
-from sqlite3 import connect
+import requests
+from bs4 import BeautifulSoup
+from sqlite3 import connect, IntegrityError
 
-conn  = connect('vhdb.sqlite3')
+headers = {'User-Agent': 'Mozilla/5.0'}
+conn = connect('vhdb.sqlite3')
 curr = conn.cursor()
 
-query = '''select * from series_record'''
-curr.execute(query)
 
-for r in curr.fetchall():
-    url = r[2]
-    soup = Bea
+def insertmatchlist(seriesrow):
+
+    linkpref = 'https://www.cricbuzz.com/live-cricket-scorecard'
+    query = '''INSERT INTO MATCHES_INSERIES(series_code, match_code, team1, team2, result, file_path)
+               VALUES(?, ?, ?, ?, ?, ?)'''
+    html = seriesrow[3]
+
+    # print(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    matches = soup.find_all('div',class_= "cb-col-75 cb-col")
+
+    # print(len(matches))
+
+    for match in matches[:]:
+        result = match.find('a', class_="cb-text-complete").get_text()
+        m = match.find('a', class_="text-hvr-underline")
+        url = m.get('href')
+        # print(url)
+        matchcode = url[16:]
+        matchcode = matchcode[:matchcode.find('/')]
+
+        url = linkpref + url[15:]
+        # print(url)
+
+        name = m.get_text()
+        name = name[:name.find(',')].split(' vs ')
+        # print(matchcode)
+
+        try:
+            ptitle = 'scorecards/' + str(seriesrow[0]) + '_' + matchcode + '.html'
+            args = (seriesrow[0], matchcode, name[0], name[1], result, ptitle)
+
+            curr.execute(query, args)
+
+            phtml = requests.get(url).text
+            f = open(ptitle, mode="w", encoding='utf8')
+            f.write(phtml)
+
+            print(matchcode, "- RECORD INSERTED!!")
+
+        except IntegrityError as e:
+            # print(e)
+            print(matchcode, "- SKIPPING.. RECORD ALREADY EXIST")
+
+
+def driver_insermatchlist():
+    curr.execute('select * from series_record2')
+    rows = curr.fetchall()
+
+    for row in rows[:]:
+        print(" " * 17 + "SERIES CODE - {}".format(row[0]) + " " * 20)
+        print("-"*50)
+        insertmatchlist(row)
+        conn.commit()
+        print("-" * 50)
+
+driver_insermatchlist()
--- matchlist.py
+++ matchlist.py
@@ -1,26 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-from sqlite3 import connect
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-conn  = connect('vhdb.sqlite3')
-curr = conn.cursor()
-
-query = '''select * from series_record'''
-curr.execute(query)
-
-iquery = 'insert into series_record2 values(?, ?, ?, ?)'
-
-for r in curr.fetchall()[:]:
-    url = r[2]
-    print(url)
-    html = requests.get(url, headers).text
-
-    args = (r[0], r[1], r[2], html)
-    curr.execute(iquery, args)
-
-conn.commit()
-
-
-
-# def getmatchlist(year):
+getmatchlist('url')
